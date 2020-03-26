6a017e288b7c9697a8fed769f9923eafdd0f8669
yoonje <elxm6123@gmail.com>
2020-03-27 04:08:53 +0900

    add 2167 1932 11055

---
+++ BOJ/1932.py
@@ -0,0 +1,14 @@
+N = int(input())
+A = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+dp = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    tmp = list(map(int, input().split()))
+    for j in range(1, i + 1):
+        A[i][j] = tmp[j - 1]
+
+for i in range(1, N + 1):
+    for j in range(1, i + 1):
+        dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + A[i][j]
+
+print(max(dp[-1]))
