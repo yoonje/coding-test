119518a49b08acaf292d5c16ba2dbb06d276d259
yoonje <yoonjechoi.tech@gmail.com>
2020-04-02 04:27:06 +0900

    Refactorying 1.py 2019

--- Kakao2019/1.py
+++ Kakao2019/1.py
@@ -1,20 +1,12 @@
-def update_uid_table(item, uid_table):
-    if item[0] == "Enter" and (uid_table.get(item[1]) is None):
-        uid_table[item[1]] = item[2]
-    elif item[0] == "Change" and (uid_table.get(item[1]) is None):
-        uid_table[item[1]] = item[2]
-    return uid_table
-
-
 def solution(record):
-    uid_table = {}
     answer = []
-    for i in range(len(record) - 1, -1, -1):
-        uid_table = update_uid_table(record[i].split(), uid_table)
-    for j in range(0, len(record)):
-        record_list = record[j].split()
-        if record_list[0] == "Enter":
-            answer.append(uid_table[record_list[1]] + "님이 들어왔습니다.")
-        elif record_list[0] == "Leave":
-            answer.append(uid_table[record_list[1]] + "님이 나갔습니다.")
+    user_list = {}
+    for i in record:
+        if i.split(' ')[0] == 'Enter' or i.split(' ')[0] == 'Change':
+            user_list[i.split(' ')[1]] = i.split(' ')[2]
+    for i in record:
+        if i.split(' ')[0] == 'Enter':
+            answer.append(user_list[i.split(' ')[1]] + '님이 들어왔습니다.')
+        elif i.split(' ')[0] == 'Leave':
+            answer.append(user_list[i.split(' ')[1]] + '님이 나갔습니다.')
     return answer
