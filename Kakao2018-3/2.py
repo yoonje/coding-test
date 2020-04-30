771e70bdd2ae6fe59613f6cb0e55f7a9c19b0591
yoonje <yoonjechoi.tech@gmail.com>
2020-05-01 03:27:06 +0900

    fix unusable code

--- Kakao2018-3/2.py
+++ Kakao2018-3/2.py
@@ -17,8 +17,3 @@ def solution(msg):
                 msg = msg[len(msg)-i:]
                 break
     return answer
-
-
-print(solution("KAKAO"))  # [11, 1, 27, 15]
-print(solution("TOBEORNOTTOBEORTOBEORNOT"))  # [20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34]
-# print(solution("ABABABABABABABAB"))  # [1, 2, 27, 29, 28, 31, 30]
