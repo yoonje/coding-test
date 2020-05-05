e0d38a6337190c4c545126368a34b844562c537e
yoonje <elxm6123@gmail.com>
2020-05-05 17:49:04 +0900

    refactory problem2 2020 winter internship kakao

--- Kakao2020-winter-internship/2.py
+++ Kakao2020-winter-internship/2.py
@@ -1,18 +1,12 @@
-import copy
-
-
 def solution(s):
-    s = s[2:len(s) - 2].split("},{")
-    set_list = list()
-    for item in s:
-        set_list.append(list(map(int, (item.split(",")))))
-    set_list.sort(key=lambda x: len(x))
-    for idx in range(1, len(set_list)):
-        if set(set_list[idx]).issuperset(set(set_list[idx - 1])) is True:
-            item = copy.deepcopy(set_list[idx])
-            for temp in set_list[idx - 1]:
-                item.remove(temp)
-            val = copy.deepcopy(set_list[idx - 1])
-            val.extend(item)
-            set_list[idx] = val
-    return set_list[-1]
+    s = s[2:-2].split("},{")
+    sorted_s = sorted(s, key=lambda x: len(x))
+    answer = []
+    check = set()
+    for value in sorted_s:
+        for each_value in value.split(","):
+            if each_value not in check:
+                answer.append(int(each_value))
+                check.add(each_value)
+                break
+    return answer
