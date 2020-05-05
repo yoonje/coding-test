fa9de6aadd01172edbb73ce980347016dae59c9a
yoonje <elxm6123@gmail.com>
2020-05-05 23:51:13 +0900

    refactory problem3 2020 winter internship kakao

---
+++ Kakao2020-winter-internship/3.py
@@ -0,0 +1,18 @@
+import re
+from itertools import permutations
+
+
+def solution(user_id, banned_id):
+    banned_id = [item.replace("*", ".") for item in banned_id]
+    answer = []
+    user_len, banned_len = len(user_id), len(banned_id)
+    permuted = permutations(user_id, banned_len)
+    for user_arr in permuted:
+        cnt = 0
+        for i in range(banned_len):
+            if re.search("^" + banned_id[i] + "$", user_arr[i]):
+                cnt += 1
+        sorted_user = sorted(user_arr)
+        if cnt == banned_len and not sorted_user in answer:
+            answer.append(sorted_user)
+    return len(answer)
